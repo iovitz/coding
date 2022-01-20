15d90b1ee029c678ebe752689153f052591b6a93
iovitz <iovitz1999@gmail.com>
2022-01-20 18:06:28 +0800

    数据库连接成功

--- egg：学习egg的使用/app/router.js
+++ egg：学习egg的使用/app/router.js
@@ -1,9 +1,18 @@
 'use strict';
 
-/**
- * @param {Egg.Application} app - egg application
- */
+const mongoose = require('mongoose');
+const { mongoDBUrl } = require('../config/db');
+
+// 链接数据库
+mongoose.connect(mongoDBUrl)
+  .then(() => {
+    console.log('mongoDB connected');
+  })
+  .catch(err => {
+    console.log('---', err);
+  });
+
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  router.get('/project', controller.project.getTemplate);
 };
