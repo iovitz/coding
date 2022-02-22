4d40e20812d9b1e82dddf4606e08c5b1724b7574
iovitz <iovitz1999@gmail.com>
2022-02-22 16:52:40 +0800

    学习dart

--- Dart：基础学习/02_string.dart
+++ Dart：基础学习/02_string.dart
@@ -5,8 +5,17 @@ void main() {
 useString() {
   String dogName = 'xiugou';
   String catName = 'maomao';
+  // concat string and variable
+
   String words1 = 'dog: $dogName, cat: $catName';
+
   String words2 = 'dog: ' + dogName + ', cat: ' + catName;
+
+  String words3 = '''Hi,
+Here is iovitz.
+''';
+
   print(words1);
   print(words2);
+  print(words3);
 }
