7257dc5d6c6326c8d05911214cc5fa2991792298
iovitz <iovitz1999@gmail.com>
2022-03-20 10:11:04 +0800

    chore: delete repo

--- Dart_Study/non_name_function.dart
+++
@@ -1,9 +0,0 @@
-void main(List<String> args) {
-  final res = doing(((num) => num + 10));
-  print(res);
-}
-
-typedef ParamsType = int Function(int num);
-doing(ParamsType func) {
-  return func(19);
-}
---
+++ TS_Ts设计模式/src/main.ts
@@ -0,0 +1,5 @@
+console.log(
+  JSON.stringify({
+    name: 'zhangsan'
+  }, null, '--')
+)
