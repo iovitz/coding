8113f72da584aedda9f85b46803126a2aafcc35f
iovitz <iovitz1999@gmail.com>
2022-05-10 23:27:39 +0800

    style: 添加支持

---
+++ dev/bitale-dev-helper/src/views/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './style.module.less'
+
+
+function App() {
+  return (
+  <div className={style.red} title="red">
+    <h1 className={style.red}>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
